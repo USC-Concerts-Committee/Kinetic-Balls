4bafd8ec3561a4f87cc6fa9b31d3ae630b0cd04e
john-bush <johncbus@usc.edu>
2022-03-26 06:53:49 -0700

    updated pin addresses for board1

--- kinectic-morph-balls/src/main.cpp
+++ kinectic-morph-balls/src/main.cpp
@@ -12,27 +12,26 @@
 #define stepPin_1 	5
 #define dirPin_2 	6
 #define stepPin_2 	7
-#define dirPin_3	8
-#define stepPin_3	9
-#define dirPin_4	10
-#define stepPin_4	11
-#define dirPin_5	12
-#define stepPin_5	13
+#define dirPin_3	11 //8
+#define stepPin_3	10 //9
+#define dirPin_4	13 //10
+#define stepPin_4	12 //11
+#define dirPin_5	8 //12
+#define stepPin_5	9 //13
 
-#define dirPin_6	54
-#define stepPin_6	55
-#define dirPin_7	56
-#define stepPin_7	57
-#define dirPin_8	58
-#define stepPin_8	59
-#define dirPin_9	60
-#define stepPin_9	61
-#define dirPin_10	62
-#define stepPin_10	63
-#define dirPin_11	64
-#define stepPin_11	65
-#define dirPin_12	66
-#define stepPin_12	67
+#define dirPin_6	56 //54
+#define stepPin_6	57 //55
+#define dirPin_7	58 //56
+#define stepPin_7	59 //57
+#define dirPin_8	60 //58
+#define stepPin_8	61 //59
+#define dirPin_9	62 // 60
+#define stepPin_9	63 //61
+#define dirPin_10	64 //62
+#define stepPin_10	65 //63
+#define dirPin_11	66 //64
+#define stepPin_11	67 //65
+
 
 #define MAX_SPEED 1000
 
